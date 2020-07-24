a83e19333e3dfbbd8eeba0dbf50c8c0f6941dfa4
hrish-2020 <64304395+hrish-2020@users.noreply.github.com>
2020-07-24 10:46:53 +0530

    Add files via upload

---
+++ assignment2.py
@@ -0,0 +1,44 @@
+uname1="manas"
+p1="123"
+uname2="omi"
+p2="456"
+uname3="shiva"
+p3="789"
+uname4="apurva"
+p4="101"
+uname5="pratham"
+p5="102"
+user=input("enter your username")
+if user==uname1:
+    password=input("enter the password")
+    if password==p1:
+        print(f"hey {uname1} ")
+    else:
+        print("please enter correct pass")
+elif user==uname2:
+      password1==input("enter the password")
+      if password1==p2:
+          print(f"hey {uname2} ")
+      else:
+          print("please enter correct pass")
+elif user==uname3:
+      password2=input("enter the password")
+      if password2==p3:
+          print(f"hey {uname3} ")
+      else:
+          print("please enter correct pass")
+elif user==uname4:
+      password3=input("enter the password")
+      if password3==p4:
+          print(f"hey {uname4} ")
+      else:
+          print("please enter correct pass")
+elif user==uname5:
+      password4=input("enter the password")
+      if password4==p5:
+          print(f"hey {uname5} ")
+      else:
+          print("please enter correct pass")
+else:
+    print("enter valid username")
+quit()
